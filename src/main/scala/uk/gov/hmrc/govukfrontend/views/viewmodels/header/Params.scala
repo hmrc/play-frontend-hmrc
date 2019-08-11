ba566f10196287255c1158de6f380f6475159298
Dario Abdulrehman <167463+dabd@users.noreply.github.com>
2019-08-11 18:31:13 +0100

    Add missing files

---
+++ src/main/scala/uk/gov/hmrc/govukfrontend/views/viewmodels/header/Params.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.govukfrontend.views.viewmodels.header
+
+case class Params(
+  homepageUrl: Option[String]       = None,
+  assetsPath: Option[String]        = None,
+  productName: Option[String]       = None,
+  serviceName: Option[String]       = None,
+  serviceUrl: Option[String]        = None,
+  navigation: Seq[Navigation]       = Nil,
+  navigationClasses: Option[String] = None,
+  containerClasses: Option[String]  = None,
+  classes: Option[String]           = None,
+  attributes: Map[String, String]   = Map.empty
+)
