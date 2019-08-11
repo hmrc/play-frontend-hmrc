98238471737b6accd03a33807fa860adca401e09
Dario Abdulrehman <167463+dabd@users.noreply.github.com>
2019-08-12 00:22:55 +0100

    Add meta-build files

---
+++ project/project/PlayCrossCompilation.scala
@@ -0,0 +1,12 @@
+
+import uk.gov.hmrc.playcrosscompilation.AbstractPlayCrossCompilation
+import uk.gov.hmrc.playcrosscompilation.PlayVersion._
+
+object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play26) {
+  val playRevision = PlayCrossCompilation.playVersion match {
+    case Play25 => "2.5.19"
+    case Play26 => "2.6.23"
+  }
+
+  val sbtPlayCrossCompilationVersion = "0.17.0"
+}
---
+++ project/project/plugins.sbt
@@ -0,0 +1,4 @@
+resolvers += Resolver.url("hmrc-sbt-plugin-releases", url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(
+  Resolver.ivyStylePatterns)
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-play-cross-compilation" % "0.17.0")
