8ae16ae087c66bc795cf67ce3fd54e857c828838
Dario Abdulrehman <167463+dabd@users.noreply.github.com>
2019-08-20 19:18:53 +0100

    Refactor
    - Don't use Play auto-generated symbols when exporting Play 2.5 components
    - Export types for Play 2.6 components

--- src/main/play-25/uk/gov/hmrc/govukfrontend/views/html/components/package.scala
+++ src/main/play-25/uk/gov/hmrc/govukfrontend/views/html/components/package.scala
@@ -17,51 +17,36 @@
 package uk.gov.hmrc.govukfrontend.views.html
 
 import uk.gov.hmrc.govukfrontend.views.{Aliases, Utils}
-import uk.gov.hmrc.govukfrontend.views.html.components.backLink_Scope0.backLink
-import uk.gov.hmrc.govukfrontend.views.html.components.button_Scope0.button
-import uk.gov.hmrc.govukfrontend.views.html.components.details_Scope0.details
-import uk.gov.hmrc.govukfrontend.views.html.components.errorMessage_Scope0.errorMessage
-import uk.gov.hmrc.govukfrontend.views.html.components.errorSummary_Scope0.errorSummary
-import uk.gov.hmrc.govukfrontend.views.html.components.fieldset_Scope0.fieldset
-import uk.gov.hmrc.govukfrontend.views.html.components.footer_Scope0.footer
-import uk.gov.hmrc.govukfrontend.views.html.components.govukTemplate_Scope0.govukTemplate_Scope1.govukTemplate
-import uk.gov.hmrc.govukfrontend.views.html.components.header_Scope0.header_Scope1.header
-import uk.gov.hmrc.govukfrontend.views.html.components.hint_Scope0.hint
-import uk.gov.hmrc.govukfrontend.views.html.components.label_Scope0.label
-import uk.gov.hmrc.govukfrontend.views.html.components.phaseBanner_Scope0.phaseBanner
-import uk.gov.hmrc.govukfrontend.views.html.components.radios_Scope0.radios
-import uk.gov.hmrc.govukfrontend.views.html.components.skipLink_Scope0.skipLink
-import uk.gov.hmrc.govukfrontend.views.html.components.tag_Scope0.tag
 
 package object components extends Utils with Aliases {
 
-  lazy val BackLink = new backLink()
+  lazy val BackLink = backLink
 
-  lazy val Button = new button()
+  lazy val Button = button
 
-  lazy val ErrorSummary = new errorSummary()
+  lazy val ErrorSummary = errorSummary
 
-  lazy val Fieldset = new fieldset()
+  lazy val Fieldset = fieldset
 
-  lazy val Footer = new footer()
+  lazy val Footer = footer
 
-  lazy val Header = new header()
+  lazy val Header = header
 
-  lazy val Hint = new hint()
+  lazy val Hint = hint
 
-  lazy val Label = new label()
+  lazy val Label = label
 
-  lazy val Tag = new tag()
+  lazy val Tag = tag
 
-  lazy val PhaseBanner = new phaseBanner()
+  lazy val PhaseBanner = phaseBanner
 
-  lazy val SkipLink = new skipLink()
+  lazy val SkipLink = skipLink
 
-  lazy val ErrorMessage = new errorMessage()
+  lazy val ErrorMessage = errorMessage
 
-  lazy val Details = new details()
+  lazy val Details = details
 
-  lazy val Radios = new radios()
+  lazy val Radios = radios
 
-  lazy val GovukTemplate = new govukTemplate()
+  lazy val GovukTemplate = govukTemplate
 }
