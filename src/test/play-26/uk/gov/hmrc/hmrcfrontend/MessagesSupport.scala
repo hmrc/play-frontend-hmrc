7daaaaa15ab7170380c4bf008817c2bdb2fc8001
Matthew Mascord <27346863+matthewmascord@users.noreply.github.com>
2020-09-08 20:32:06 +0100

    PLATUI-703: Add HMRC footer component

---
+++ src/test/play-26/uk/gov/hmrc/hmrcfrontend/MessagesSupport.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hmrcfrontend
+
+import play.api.http.HttpConfiguration
+import play.api.{Configuration, Environment}
+import play.api.i18n._
+import play.api.mvc.MessagesRequest
+import play.api.test.FakeRequest
+
+trait MessagesSupport {
+
+  implicit lazy val messagesApi: MessagesApi = {
+
+    val environment = Environment.simple()
+
+    val configuration = Configuration.load(environment)
+
+    val langs = new DefaultLangs(Seq(Lang("en"), Lang("cy")))
+
+    new DefaultMessagesApiProvider(
+      environment       = environment,
+      config            = configuration,
+      langs             = langs,
+      httpConfiguration = new HttpConfiguration()
+    ).get
+  }
+
+  implicit val messages: Messages = new MessagesRequest(FakeRequest(), messagesApi).messages
+}
---
+++ src/test/scala/uk/gov/hmrc/hmrcfrontend/views/config/HmrcFooterItemsSpec.scala
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hmrcfrontend.views.config
+
+import org.scalatest.{MustMatchers, WordSpec}
+import play.api.Application
+import play.api.i18n.{Lang, Messages}
+import play.api.test.FakeRequest
+import play.api.inject.guice.GuiceApplicationBuilder
+import uk.gov.hmrc.govukfrontend.views.viewmodels.footer.FooterItem
+import uk.gov.hmrc.hmrcfrontend.MessagesSupport
+
+class HmrcFooterItemsSpec extends WordSpec with MustMatchers with MessagesSupport {
+  implicit val request: FakeRequest[_] = FakeRequest("GET", "/foo")
+
+  def buildApp(properties: Map[String, String] = Map.empty): Application =
+    new GuiceApplicationBuilder()
+      .configure(properties)
+      .build()
+
+  "HmrcFooterItems" must {
+    "Return the correct links for the item keys defined" in {
+      val app             = buildApp()
+      val hmrcFooterItems = app.injector.instanceOf[HmrcFooterItems]
+
+      hmrcFooterItems.get mustBe Seq(
+        FooterItem(
+          Some("Cookies"),
+          Some("/help/cookies")
+        ),
+        FooterItem(
+          Some("Privacy policy"),
+          Some("/help/privacy")
+        ),
+        FooterItem(
+          Some("Terms and conditions"),
+          Some("/help/terms-and-conditions")
+        ),
+        FooterItem(
+          Some("Help using GOV.UK"),
+          Some("https://www.gov.uk/help")
+        )
+      )
+    }
+
+    "Return the accessibility link if defined" in {
+      val app = buildApp(
+        Map(
+          "platform.frontend.host"               -> "https://www.tax.service.gov.uk",
+          "accessibility-statement.service-path" -> "/bar"
+        ))
+      val hmrcFooterItems = app.injector.instanceOf[HmrcFooterItems]
+
+      hmrcFooterItems.get mustBe Seq(
+        FooterItem(
+          Some("Cookies"),
+          Some("/help/cookies")
+        ),
+        FooterItem(
+          Some("Accessibility statement"),
+          Some(
+            "https://www.tax.service.gov.uk/accessibility-statement/bar?referrerUrl=https%3A%2F%2Fwww.tax.service.gov.uk%2Ffoo")
+        ),
+        FooterItem(
+          Some("Privacy policy"),
+          Some("/help/privacy")
+        ),
+        FooterItem(
+          Some("Terms and conditions"),
+          Some("/help/terms-and-conditions")
+        ),
+        FooterItem(
+          Some("Help using GOV.UK"),
+          Some("https://www.gov.uk/help")
+        )
+      )
+    }
+
+    "Return the correct links in Welsh" in {
+      val app                         = buildApp()
+      implicit val messages: Messages = messagesApi.preferred(Seq(Lang("cy")))
+      val hmrcFooterItems             = app.injector.instanceOf[HmrcFooterItems]
+
+      hmrcFooterItems.get mustBe Seq(
+        FooterItem(
+          Some("Cwcis"),
+          Some("/help/cookies")
+        ),
+        FooterItem(
+          Some("Polisi preifatrwydd"),
+          Some("/help/privacy")
+        ),
+        FooterItem(
+          Some("Telerau ac Amodau"),
+          Some("/help/terms-and-conditions")
+        ),
+        FooterItem(
+          Some("Help wrth ddefnyddio GOV.UK"),
+          Some("https://www.gov.uk/help")
+        )
+      )
+    }
+
+    "Return the correct links in Welsh including the accessibility statement link" in {
+      val app = buildApp(
+        Map(
+          "platform.frontend.host"               -> "https://www.tax.service.gov.uk",
+          "accessibility-statement.service-path" -> "/bar"
+        ))
+      implicit val messages: Messages = messagesApi.preferred(Seq(Lang("cy")))
+      val hmrcFooterItems             = app.injector.instanceOf[HmrcFooterItems]
+
+      hmrcFooterItems.get mustBe Seq(
+        FooterItem(
+          Some("Cwcis"),
+          Some("/help/cookies")
+        ),
+        FooterItem(
+          Some("Datganiad hygyrchedd"),
+          Some(
+            "https://www.tax.service.gov.uk/accessibility-statement/bar?referrerUrl=https%3A%2F%2Fwww.tax.service.gov.uk%2Ffoo")
+        ),
+        FooterItem(
+          Some("Polisi preifatrwydd"),
+          Some("/help/privacy")
+        ),
+        FooterItem(
+          Some("Telerau ac Amodau"),
+          Some("/help/terms-and-conditions")
+        ),
+        FooterItem(
+          Some("Help wrth ddefnyddio GOV.UK"),
+          Some("https://www.gov.uk/help")
+        )
+      )
+    }
+  }
+}
