fe0cab9a06ab02f10a6a0bf6bf3c3f792c6dd4eb
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2021-01-08 11:19:32 +0000

    PLATUI-795: Updated match statement for Play cross-compilation

--- project/project/PlayCrossCompilation.scala
+++ project/project/PlayCrossCompilation.scala
@@ -3,8 +3,9 @@ import uk.gov.hmrc.playcrosscompilation.PlayVersion._
 
 object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play27) {
   val playRevision: String = PlayCrossCompilation.playVersion match {
-    case Play26 => "2.6.23"
     case Play27 => "2.7.5"
+    case Play26 => "2.6.23"
+    case Play25 => throw new IllegalArgumentException("Play 2.5 is no longer supported by this library")
   }
 
   val sbtPlayCrossCompilationVersion = "0.20.0"
