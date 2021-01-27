d5983b9d2c6693a581785bee5772512e1876e71f
Matthew Mascord <27346863+matthewmascord@users.noreply.github.com>
2021-01-27 17:18:19 +0000

    PLATUI-935: Add hmrcHead and hmrcScript helpers

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -4,7 +4,7 @@ import play.sbt.PlayImport.ws
 import sbt.ModuleID
 
 object LibDependencies {
-  lazy val hmrcFrontendVersion = "1.23.1"
+  lazy val hmrcFrontendVersion = "1.24.0"
   private val playFrontendGovukVersion = "0.60.0"
 
   lazy val libDependencies: Seq[ModuleID] = dependencies(
---
+++ src/main/scala/uk/gov/hmrc/hmrcfrontend/config/AssetsConfig.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hmrcfrontend.config
+
+import javax.inject.Inject
+import uk.gov.hmrc.hmrcfrontend.controllers.routes.Assets
+import uk.gov.hmrc.hmrcfrontend.views.HmrcFrontendDependency
+
+class AssetsConfig @Inject()() {
+  private val hmrcFrontendVersion = HmrcFrontendDependency.hmrcFrontendVersion
+
+  val html5ShivJsUrl: String = Assets.at("vendor/html5shiv.min.js").url
+  val hmrcFrontendCssUrl: String = hmrcFrontendAssetUrl(s"hmrc-frontend-$hmrcFrontendVersion.min.css")
+  val hmrcFrontendIe8CssUrl: String = hmrcFrontendAssetUrl(s"hmrc-frontend-ie8-$hmrcFrontendVersion.min.css")
+  val hmrcFrontendJsUrl: String = hmrcFrontendAssetUrl(s"hmrc-frontend-$hmrcFrontendVersion.min.js")
+
+  private def hmrcFrontendAssetUrl(filename: String) = Assets.at(filename).url
+}
