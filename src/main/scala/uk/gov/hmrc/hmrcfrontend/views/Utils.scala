40ba1adff9242d7b12de723bbe488fdd19e843ca
Kyle Bowden <1393788+kyle-bowden@users.noreply.github.com>
2021-12-03 09:00:23 +0000

    Add internal SVG in HmrcInternalHeader & HmrcBanner

--- src/main/scala/uk/gov/hmrc/hmrcfrontend/views/Utils.scala
+++ src/main/scala/uk/gov/hmrc/hmrcfrontend/views/Utils.scala
@@ -18,6 +18,12 @@ package uk.gov.hmrc.hmrcfrontend.views
 
 import uk.gov.hmrc.supportfrontend.views.UtilsSupport
 
-trait Utils extends UtilsSupport
+trait Utils extends UtilsSupport {
+
+  private[views] def calculateAssetPath(path: Option[String], file: String): String =
+    path
+      .map(p => s"$p/$file")
+      .getOrElse(uk.gov.hmrc.hmrcfrontend.controllers.routes.Assets.at(file).url)
+}
 
 object Utils extends Utils
