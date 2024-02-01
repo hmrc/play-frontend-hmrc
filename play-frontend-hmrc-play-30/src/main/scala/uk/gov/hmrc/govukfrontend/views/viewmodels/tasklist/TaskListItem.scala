b9715fdbb1bf681401bb8740205cdb1fc9190897
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-02-01 09:30:11 +0000

    PLATUI-2706: Updated writes

--- play-frontend-hmrc-play-30/src/main/scala/uk/gov/hmrc/govukfrontend/views/viewmodels/tasklist/TaskListItem.scala
+++ play-frontend-hmrc-play-30/src/main/scala/uk/gov/hmrc/govukfrontend/views/viewmodels/tasklist/TaskListItem.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.govukfrontend.views.viewmodels.tasklist
 
 import uk.gov.hmrc.govukfrontend.views.viewmodels.content.{Content, Empty}
@@ -16,7 +32,7 @@ case class TaskListItem(
 object TaskListItem {
     def defaultObject: TaskListItem = TaskListItem()
 
-    implicit def jsonReads: Reads[TaskListItem] = 
+    implicit def jsonReads: Reads[TaskListItem] =
         (
             (__ \ "title").readWithDefault[TaskListItemTitle](defaultObject.title) and
                 (__ \ "hint").readWithDefault[Content](defaultObject.hint) and
@@ -30,7 +46,7 @@ object TaskListItem {
             (__ \ "title").write[TaskListItemTitle] and
                 (__ \ "hint").write[Content] and
                 (__ \ "status").write[TaskListItemStatus] and
-                (__ \ "href").writeNullable[String] and
-                (__ \ "classes").writeNullable[String]        
+                (__ \ "href").write[String] and
+                (__ \ "classes").write[String]
         )(unlift(TaskListItem.unapply))
 }
