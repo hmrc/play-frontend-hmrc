84ac48f070ee08d2f42afac9c86070f902dcba89
david.ellam <ellamdav@users.noreply.github.com>
2024-02-09 15:05:39 +0000

    PLATUI-2706 bump hmrc-frontend to latest

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object LibDependencies {
   val govukFrontendVersion: String = "5.0.0"
-  val hmrcFrontendVersion: String  = "6.0.0"
+  val hmrcFrontendVersion: String  = "6.1.0"
   val playLanguageVersion: String  = "7.0.0"
 
   val play28Version = "2.8.20"
