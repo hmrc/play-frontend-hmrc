8d3e7ff3a662b26fd01ce2384387b76efa73a085
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-03-06 16:14:21 +0000

    PLATUI-2830: Refactored HmrcFooter to delegate to GovukFooter

--- play-frontend-hmrc-play-30/src/test/scala/uk/gov/hmrc/hmrcfrontend/views/components/HmrcFooterSpec.scala
+++ play-frontend-hmrc-play-30/src/test/scala/uk/gov/hmrc/hmrcfrontend/views/components/HmrcFooterSpec.scala
@@ -17,6 +17,22 @@
 package uk.gov.hmrc.hmrcfrontend.views
 package components
 
+import play.api.mvc.RequestHeader
+import play.api.test.FakeRequest
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.helpers.MessagesSupport
 import uk.gov.hmrc.hmrcfrontend.views.html.components._
 
-class HmrcFooterSpec extends TemplateUnitSpec[Footer, HmrcFooter]("hmrcFooter")
+import scala.util.Try
+
+
+class HmrcFooterSpec extends TemplateUnitBaseSpec[Footer]("hmrcFooter") with MessagesSupport {
+
+  private val component = app.injector.instanceOf[HmrcFooter]
+
+  def render(templateParams: Footer): Try[HtmlFormat.Appendable] = {
+    implicit val request: RequestHeader = FakeRequest("GET", "/foo")
+
+    Try(component(templateParams))
+  }
+}
