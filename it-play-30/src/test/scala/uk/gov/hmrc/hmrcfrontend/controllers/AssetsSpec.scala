841a3123dae69c5223ba33e90e209a5dbe0094bd
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2024-04-18 13:24:46 +0100

    PLATUI-2703 add test for cache-control headers

--- it-play-30/src/test/scala/uk/gov/hmrc/hmrcfrontend/controllers/AssetsSpec.scala
+++ it-play-30/src/test/scala/uk/gov/hmrc/hmrcfrontend/controllers/AssetsSpec.scala
@@ -18,6 +18,7 @@ package uk.gov.hmrc.hmrcfrontend.controllers
 
 import org.scalatest.TestData
 import org.scalatest.matchers.must.Matchers
+import org.scalatest.matchers._
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.Application
@@ -36,6 +37,11 @@ class AssetsSpec extends AnyWordSpec with Matchers with Results with GuiceOneApp
   "Asset controller " must {
     "serve an asset in the public assets folder" in {
       status(route(app, FakeRequest("GET", "/assets/all.js")).get) must be(200)
+    }
+  }
+  "Asset controller " must {
+    "serve assets with correct cache-control headers" in {
+      header("Cache-Control", route(app, FakeRequest("GET", "/assets/all.js")).get).get must include ("max-age=3600")
     }
   }
 
