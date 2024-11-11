6b5eded6ee7469d0b203bbd2c19265f54486dc78
Joanna Pinto Paul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-11-11 13:14:15 +0000

    PLATUI-3341: Updated hmrc-frontend version to include webjar built wi… (#331)
    PLATUI-3341: Updated hmrc-frontend version to include webjar built with gulp 5

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object LibDependencies {
   val govukFrontendVersion: String = "5.7.1"
-  val hmrcFrontendVersion: String  = "6.37.0"
+  val hmrcFrontendVersion: String  = "6.38.0"
   val playLanguageVersion: String  = "8.1.0"
 
   val play28Version = "2.8.22"
