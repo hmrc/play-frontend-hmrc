9014fdc5488e70362dea3d309e6ac12a3ab93084
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2025-01-21 11:37:20 +0000

    PLATUI-3352: update hmrc-frontend to 6.53.0

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object LibDependencies {
   val govukFrontendVersion: String = "5.8.0"
-  val hmrcFrontendVersion: String  = "6.52.0"
+  val hmrcFrontendVersion: String  = "6.53.0"
   val playLanguageVersion: String  = "8.1.0"
 
   val play28Version = "2.8.22"
