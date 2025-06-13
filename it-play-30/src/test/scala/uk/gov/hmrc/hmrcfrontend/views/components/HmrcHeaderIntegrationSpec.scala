29fec4aabb3041158f2f856982147bc2c36ab6e3
Joanna Pinto Paul <joanna.pinto.paul@digital.hmrc.gov.uk>
2025-06-13 09:02:52 +0100

    PLATUI-3794: Refactored HmrcHeader to use new HeaderParams viewmodel (#354)
    PLATUI-3794: Migrated to new HeaderParams viewmodel for HmrcHeader including HmrcStandardHeader

--- it-play-30/src/test/scala/uk/gov/hmrc/hmrcfrontend/views/components/HmrcHeaderIntegrationSpec.scala
+++ it-play-30/src/test/scala/uk/gov/hmrc/hmrcfrontend/views/components/HmrcHeaderIntegrationSpec.scala
@@ -24,35 +24,34 @@ import uk.gov.hmrc.hmrcfrontend.views.html.components._
 // model of Header is less flexible and specifically requests the href for `cy`
 // and `en` to be defined.
 import uk.gov.hmrc.hmrcfrontend.views.viewmodels.header.Generators._
-import uk.gov.hmrc.hmrcfrontend.views.viewmodels.header.Header
 import uk.gov.hmrc.hmrcfrontend.views.viewmodels.language.En
 
 object HmrcHeaderIntegrationSpec
-    extends TemplateIntegrationSpec[Header, HmrcHeader](hmrcComponentName = "hmrcHeader", seed = None) {
+    extends TemplateIntegrationSpec[HeaderParams, HmrcHeader](hmrcComponentName = "hmrcHeader", seed = None) {
 
-  override def classifiers(header: Header): LazyList[ClassifyParams] =
-    (header.homepageUrl.isEmpty, "empty homepageUrl", "non-empty homepageUrl") #::
-      (header.homepageUrl.length > 10, "long homepageUrl", "short homepageUrl") #::
-      (header.assetsPath.isEmpty, "empty assetsPath", "non-empty assetsPath") #::
-      (header.assetsPath.length > 10, "long assetsPath", "short assetsPath") #::
-      (header.productName.isEmpty, "empty productName", "non-empty productName") #::
-      (header.productName.exists(_.length > 10), "long productName", "short productName") #::
-      (header.serviceName.isEmpty, "empty serviceName", "non-empty serviceName") #::
-      (header.serviceName.exists(_.length > 10), "long serviceName", "short serviceName") #::
-      (header.serviceUrl.isEmpty, "empty serviceUrl", "non-empty serviceUrl") #::
-      (header.serviceUrl.length > 10, "long serviceUrl", "short serviceUrl") #::
-      (header.navigation.isEmpty, "empty navigation list", "non-empty navigation list") #::
-      (header.navigation.exists(_.length > 10), "long navigation list", "short navigation list") #::
-      (header.navigationClasses.isEmpty, "empty navigationClasses", "non-empty navigationClasses") #::
-      (header.navigationClasses.length > 10, "long navigationClasses", "short navigationClasses") #::
-      (header.classes.isEmpty, "empty classes", "non-empty classes") #::
-      (header.classes.length > 10, "long classes", "short classes") #::
-      (header.attributes.isEmpty, "empty attributes", "non-empty attributes") #::
-      (header.attributes.values.exists(_.isEmpty), "empty attributes values", "non-empty attributes values") #::
+  override def classifiers(header: HeaderParams): LazyList[ClassifyParams] =
+    (header.headerUrls.homepageUrl.isEmpty, "empty homepageUrl", "non-empty homepageUrl") #::
+      (header.headerUrls.homepageUrl.length > 10, "long homepageUrl", "short homepageUrl") #::
+      (header.headerUrls.assetsPath.isEmpty, "empty assetsPath", "non-empty assetsPath") #::
+      (header.headerUrls.assetsPath.length > 10, "long assetsPath", "short assetsPath") #::
+      (header.headerNames.productName.isEmpty, "empty productName", "non-empty productName") #::
+      (header.headerNames.productName.exists(_.length > 10), "long productName", "short productName") #::
+      (header.headerNames.serviceName.isEmpty, "empty serviceName", "non-empty serviceName") #::
+      (header.headerNames.serviceName.exists(_.length > 10), "long serviceName", "short serviceName") #::
+      (header.headerUrls.serviceUrl.isEmpty, "empty serviceUrl", "non-empty serviceUrl") #::
+      (header.headerUrls.serviceUrl.length > 10, "long serviceUrl", "short serviceUrl") #::
+      (header.headerNavigation.navigation.isEmpty, "empty navigation list", "non-empty navigation list") #::
+      (header.headerNavigation.navigation.exists(_.length > 10), "long navigation list", "short navigation list") #::
+      (header.headerNavigation.navigationClasses.isEmpty, "empty navigationClasses", "non-empty navigationClasses") #::
+      (header.headerNavigation.navigationClasses.length > 10, "long navigationClasses", "short navigationClasses") #::
+      (header.headerTemplateOverrides.classes.isEmpty, "empty classes", "non-empty classes") #::
+      (header.headerTemplateOverrides.classes.length > 10, "long classes", "short classes") #::
+      (header.headerTemplateOverrides.attributes.isEmpty, "empty attributes", "non-empty attributes") #::
+      (header.headerTemplateOverrides.attributes.values.exists(_.isEmpty), "empty attributes values", "non-empty attributes values") #::
       (header.language == En, "language English", "language Cymraeg") #::
-      (header.displayHmrcBanner, "displayHmrcBanner true", "displayHmrcBanner false") #::
-      (header.signOutHref.isEmpty, "empty signOutHref", "non-empty signOutHref") #::
-      (header.signOutHref.exists(_.length > 10), "long signOutHref", "short signOutHref") #::
-      (header.additionalBannersBlock.isEmpty, "no additional banners", "with additional banners") #::
+      (header.banners.displayHmrcBanner, "displayHmrcBanner true", "displayHmrcBanner false") #::
+      (header.headerUrls.signOutHref.isEmpty, "empty signOutHref", "non-empty signOutHref") #::
+      (header.headerUrls.signOutHref.exists(_.length > 10), "long signOutHref", "short signOutHref") #::
+      (header.banners.additionalBannersBlock.isEmpty, "no additional banners", "with additional banners") #::
       LazyList.empty[ClassifyParams]
 }
