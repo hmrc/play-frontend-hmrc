6fbae35e3c4b3c5de8260d9c4ea79fae50387a3f
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2026-08-10 12:02:52 +0100

    PLATUI-4107: Evict play-language

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -7,20 +7,21 @@ object LibDependencies {
   val play30Version: String = "3.0.10"
 
   val play30: Seq[ModuleID] = Seq(
-    "org.playframework"            %% "play"                 % play30Version,
-    "org.playframework"            %% "play-filters-helpers" % play30Version,
-    "uk.gov.hmrc.webjars"           % "hmrc-frontend"        % hmrcFrontendVersion,
-    "com.ibm.icu"                   % "icu4j"                % "69.1",
-    "org.jsoup"                     % "jsoup"                % "1.13.1"      % Test,
-    "org.scalatestplus"            %% "mockito-3-4"          % "3.2.10.0"    % Test,
-    "org.scalatestplus"            %% "scalacheck-1-16"      % "3.2.14.0"    % Test,
-    "com.googlecode.htmlcompressor" % "htmlcompressor"       % "1.5.2"       % Test,
-    "com.github.pathikrit"         %% "better-files"         % "3.9.2"       % Test,
-    "com.lihaoyi"                  %% "pprint"               % "0.6.6"       % Test,
-    "org.bitbucket.cowwoc"          % "diff-match-patch"     % "1.2"         % Test,
-    "com.vladsch.flexmark"          % "flexmark-all"         % "0.64.8"      % Test,
-    "org.playframework"            %% "play-test"            % play30Version % Test,
-    "org.playframework"            %% "play-ahc-ws"          % play30Version % Test,
-    "org.scalatestplus.play"       %% "scalatestplus-play"   % "7.0.2"       % Test
+    "uk.gov.hmrc"                  %% "play-language-play-30" % "10.0.0",
+    "org.playframework"            %% "play"                  % play30Version,
+    "org.playframework"            %% "play-filters-helpers"  % play30Version,
+    "uk.gov.hmrc.webjars"           % "hmrc-frontend"         % hmrcFrontendVersion,
+    "com.ibm.icu"                   % "icu4j"                 % "69.1",
+    "org.jsoup"                     % "jsoup"                 % "1.13.1"      % Test,
+    "org.scalatestplus"            %% "mockito-3-4"           % "3.2.10.0"    % Test,
+    "org.scalatestplus"            %% "scalacheck-1-16"       % "3.2.14.0"    % Test,
+    "com.googlecode.htmlcompressor" % "htmlcompressor"        % "1.5.2"       % Test,
+    "com.github.pathikrit"         %% "better-files"          % "3.9.2"       % Test,
+    "com.lihaoyi"                  %% "pprint"                % "0.6.6"       % Test,
+    "org.bitbucket.cowwoc"          % "diff-match-patch"      % "1.2"         % Test,
+    "com.vladsch.flexmark"          % "flexmark-all"          % "0.64.8"      % Test,
+    "org.playframework"            %% "play-test"             % play30Version % Test,
+    "org.playframework"            %% "play-ahc-ws"           % play30Version % Test,
+    "org.scalatestplus.play"       %% "scalatestplus-play"    % "7.0.2"       % Test
   )
 }
